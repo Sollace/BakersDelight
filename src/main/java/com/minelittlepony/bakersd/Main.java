3f3d83b533473666678a1905ac73adfe0dd871ff
Sollace <sollacea@gmail.com>
2020-02-08 16:39:51 +0200

    Initial commit

---
+++ src/main/java/com/minelittlepony/bakersd/Main.java
@@ -0,0 +1,15 @@
+package com.minelittlepony.bakersd;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Main implements ClientModInitializer {
+
+    public static final Logger logger = LogManager.getLogger("BakersD");
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
