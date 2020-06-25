5cdd0b02c37ca0bde052d4203b867692b319f19d
Sollace <sollacea@gmail.com>
2020-06-25 15:54:21 +0200

    FabricBlockSettings is deprecated

--- src/main/java/com/minelittlepony/bakersd/BakersBlocks.java
+++ src/main/java/com/minelittlepony/bakersd/BakersBlocks.java
@@ -1,6 +1,5 @@
 package com.minelittlepony.bakersd;
 
-import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.sound.BlockSoundGroup;
@@ -13,32 +12,28 @@ import com.minelittlepony.bakersd.block.RyeBlock;
 import com.minelittlepony.bakersd.block.TallCropBlock;
 
 public interface BakersBlocks {
-    Block YEAST = register(new BoxBlock(BoxBlock.SMALL, FabricBlockSettings.of(Material.WOOD).sounds(BoxBlock.SOUND).build()), "yeast");
-    Block BAKING_SODA = register(new BoxBlock(BoxBlock.SMALL, FabricBlockSettings.of(Material.WOOD).sounds(BoxBlock.SOUND).build()), "baking_soda");
-    Block OAK_BREAD_BOARD = register(new BreadBoardBlock(FabricBlockSettings.of(Material.WOOD)
+    Block YEAST = register(new BoxBlock(BoxBlock.SMALL, Block.Settings.of(Material.WOOD).sounds(BoxBlock.SOUND)), "yeast");
+    Block BAKING_SODA = register(new BoxBlock(BoxBlock.SMALL, Block.Settings.of(Material.WOOD).sounds(BoxBlock.SOUND)), "baking_soda");
+    Block OAK_BREAD_BOARD = register(new BreadBoardBlock(Block.Settings.of(Material.WOOD)
             .sounds(BlockSoundGroup.WOOD)
-            .hardness(2)
-            .resistance(5)
-            .slipperiness(1)
-            .build()), "oak_bread_board");
+            .strength(2, 5)
+            .slipperiness(1)), "oak_bread_board");
 
-    Block RYE = register(new RyeBlock(FabricBlockSettings.of(Material.PLANT)
+    Block RYE = register(new RyeBlock(Block.Settings.of(Material.PLANT)
                         .noCollision()
                         .ticksRandomly()
                         .nonOpaque()
                         .breakInstantly()
-                        .sounds(BlockSoundGroup.CROP)
-                        .build(),
+                        .sounds(BlockSoundGroup.CROP),
                     () -> BakersItems.RYE
                 ), "rye");
 
-    Block CORN = register(new TallCropBlock(FabricBlockSettings.of(Material.PLANT)
+    Block CORN = register(new TallCropBlock(Block.Settings.of(Material.PLANT)
                         .noCollision()
                         .ticksRandomly()
                         .nonOpaque()
                         .breakInstantly()
-                        .sounds(BlockSoundGroup.CROP)
-                        .build(),
+                        .sounds(BlockSoundGroup.CROP),
                     () -> BakersItems.CORN
                 ), "corn");
 
